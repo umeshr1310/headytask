241ff148c2b7b248d0dee95048e796a750b71786
Umesh <r.umeshr84@gmail.com>
2018-11-02 23:00:52 +0530

    second commit

--- app/src/main/java/com/headytask/headytask/MainActivity.java
+++ app/src/main/java/com/headytask/headytask/MainActivity.java
@@ -11,18 +11,32 @@ import android.net.NetworkInfo;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.headytask.headytask.api.BackgroundSyncService;
+import com.headytask.headytask.api.Common;
 import com.headytask.headytask.localdb.OfflineCategoryDBHandler;
 import com.headytask.headytask.localdb.OfflineDBHandler;
+import com.headytask.headytask.model.TypeData;
 
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     public static final String SYNC_DATA = "com.headytask.headytask.SYNC_DATA";
 
@@ -32,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
     private List<String> ranking_product_shares_list = new ArrayList<>();
     private List<String> ranking_rankings_list = new ArrayList<>();
 
-    private List<String> category_id_list = new ArrayList<>();
+    private ArrayList<String> category_id_list = new ArrayList<>();
     private List<String> category_name_list = new ArrayList<>();
     private List<String> category_child_category_list = new ArrayList<>();
     private List<String> category_products_id_list = new ArrayList<>();
@@ -51,6 +65,7 @@ public class MainActivity extends AppCompatActivity {
     private OfflineDBHandler offlineDB;
     private OfflineCategoryDBHandler offlineCatDB;
     private boolean connectStatus = false;
+
 
     private BroadcastReceiver bReceiver = new BroadcastReceiver() {
         @Override
@@ -83,10 +98,20 @@ public class MainActivity extends AppCompatActivity {
     };
     LocalBroadcastManager bManager;
 
+    Spinner spCategorySearch,spRanking;
+    private RecyclerView recyclerView;
+    Button searchBtn;
+    String categoryTXT = "",rankingTXT = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spCategorySearch = findViewById(R.id.sp_category);
+        spRanking = findViewById(R.id.sp_ranking);
+        searchBtn = findViewById(R.id.btn_search);
+        recyclerView = findViewById(R.id.recyclerview);
 
         connectStatus = checkInternetConnection();
         System.out.println("Connection :" + connectStatus);
@@ -130,8 +155,39 @@ public class MainActivity extends AppCompatActivity {
         else {
             Toast.makeText(getApplicationContext(),"Mobile Network/Wifi is Off",Toast.LENGTH_SHORT).show();
         }
+        spCategorySearch.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                categoryTXT = parent.getItemAtPosition(position).toString().trim();
+                InputMethodManager imm = (InputMethodManager)spCategorySearch.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(spCategorySearch.getWindowToken(), 0);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                spCategorySearch.requestFocus();
+            }
+        });
+        spRanking.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                rankingTXT = parent.getItemAtPosition(position).toString().trim();
+                InputMethodManager imm = (InputMethodManager)spRanking.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(spRanking.getWindowToken(), 0);
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                spRanking.requestFocus();
+            }
+        });
+
+        searchBtn.setOnClickListener(this);
     }
+
     public boolean checkInternetConnection() {
         boolean mobileDataEnabled = false; // Assume disabled
         ConnectivityManager connManager = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
@@ -148,6 +204,23 @@ public class MainActivity extends AppCompatActivity {
             mobileDataEnabled = true;
         }
         return mobileDataEnabled;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_search :
+                if(Common.isNotNull(categoryTXT) && Common.isNotNull(rankingTXT)){
+                   /* Intent i = new Intent(MainActivity.this, ListingActivity.class);
+                    i.putExtra("CATEGORY",categoryTXT);
+                    i.putExtra("RANKING", rankingTXT);
+                    startActivity(i);*/
+                }
+                else {
+                    Toast.makeText(this,"Please select service",Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
     private void clearArrayLists() {
 
@@ -207,8 +280,20 @@ public class MainActivity extends AppCompatActivity {
             ranking_product_viewcount_list.add(view_count.trim());
             ranking_product_ordercount_list.add(order_count.trim());
             ranking_product_shares_list.add(shares.trim());
-
         }
+        ArrayAdapter categoryAdapter = new ArrayAdapter(MainActivity.this,R.layout.main_spinner_item, category_id_list);
+        categoryAdapter.setDropDownViewResource(R.layout.spinner_item);
+        spCategorySearch.setAdapter(categoryAdapter);
+
+        categoryTXT = spCategorySearch.getSelectedItem().toString();
+
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 2);
+        recyclerView.setLayoutManager(layoutManager);
+        ArrayList<TypeData> titles = prepareData(category_id_list);
+        MyDataAdapter adapter = new MyDataAdapter(getApplicationContext(), titles);
+        recyclerView.setAdapter(adapter);
+
     }
 
     private void getCategoriesDetailsDB() {
@@ -248,5 +333,105 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+    //put customized data in grid
+    private ArrayList<TypeData> prepareData(ArrayList<String> servicesList) {
 
+        System.out.println("Arraylisrt inside preparedate :"+ servicesList.size());
+
+        ArrayList<TypeData> title_list = new ArrayList<>();
+        for (int i = 0; i < servicesList.size(); i++) {
+            TypeData title = new TypeData();
+            title.setType(servicesList.get(i));
+            /*if(i<image_urls.length) {
+                title.setImg(image_urls[i]);
+            }
+            else {
+                title.setImg(R.drawable.gift);
+            }*/
+            title_list.add(title);
+        }
+        return title_list;
+    }
+    class MyDataAdapter extends RecyclerView.Adapter<MyDataAdapter.ViewHolder> {
+        private ArrayList<TypeData> list;
+        private Context context;
+
+        public MyDataAdapter(Context context, ArrayList<TypeData> list) {
+            this.list = list;
+            this.context = context;
+        }
+
+        @Override
+        public MyDataAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.custom_item, viewGroup, false);
+            return new MyDataAdapter.ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(MyDataAdapter.ViewHolder viewHolder, int i) {
+            viewHolder.tv_title.setText(list.get(i).getType());
+            viewHolder.img_icon.setImageResource(list.get(i).getImg());
+            viewHolder.setClickListener(new ItemClickListener() {
+                @Override
+                public void onClick(View view, int position, boolean isLongClick) {
+                    if (isLongClick) {
+                        //    CallIntent(position);
+                        //  Toast.makeText(context, "lClick", Toast.LENGTH_SHORT).show();
+                       /* Intent i = new Intent(MainActivity.this, ListingActivity.class);
+                        i.putExtra("SERVICE",list.get(position).getType());
+                        i.putExtra("CITY", cityTXT);
+                        i.putExtra("TALUKA", talukaTXT);
+                        startActivity(i);*/
+
+                    } else {
+                        //  CallIntent(position);
+                        //  Toast.makeText(context, "sClick", Toast.LENGTH_SHORT).show();
+                       /* Intent i = new Intent(MainActivity.this, ListingActivity.class);
+                        i.putExtra("SERVICE",list.get(position).getType());
+                        i.putExtra("CITY", cityTXT);
+                        i.putExtra("TALUKA", talukaTXT);
+                        startActivity(i);*/
+
+                    }
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
+            private TextView tv_title;
+            private ImageView img_icon;
+            private ItemClickListener clickListener;
+
+            public ViewHolder(View view) {
+                super(view);
+                tv_title = (TextView) view.findViewById(R.id.tv_title);
+                img_icon = (ImageView) view.findViewById(R.id.img_icon);
+                view.setOnClickListener(this);
+                view.setOnLongClickListener(this);
+            }
+
+            public void setClickListener(ItemClickListener itemClickListener) {
+                this.clickListener = itemClickListener;
+            }
+
+            @Override
+            public void onClick(View v) {
+                clickListener.onClick(v, getPosition(), false);
+            }
+
+            @Override
+            public boolean onLongClick(View v) {
+                clickListener.onClick(v, getPosition(), true);
+                return true;
+            }
+        }
+    }
+    public interface ItemClickListener {
+        void onClick(View view, int position, boolean isLongClick);
+    }
 }
---
+++ app/src/main/java/com/headytask/headytask/model/TypeData.java
@@ -0,0 +1,26 @@
+package com.headytask.headytask.model;
+
+/**
+ * Created by umesh on 29/03/17.
+ */
+
+public class TypeData {
+    private String type;
+    private int img;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+}
